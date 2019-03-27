891566ac3bdbfe6a7cead0168e6b3c1e967cc243
Curea Sergiu Dan <sergiucurea@WS-CJ-U2-2282.local>
2019-03-27 16:51:15 +0200

    formatting var->let

--- initializeMongoStatic.js
+++ initializeMongoStatic.js
@@ -1,11 +1,11 @@
-var getJSON = require('get-json');
-var MongoClient = require('mongoDb').MongoClient;
-var url = "mongodb://localhost:27017/";
+let getJSON = require('get-json');
+let MongoClient = require('mongoDb').MongoClient;
+let url = "mongodb://localhost:27017/";
 let i = 1;
 let index = 0;
 
 MongoClient.connect(url, function (err, db) {
-    var dbo = db.db('movieDB');
+    let dbo = db.db('movieDB');
     dbo.dropCollection("movies", function (err, delOK) {
         if (delOK) console.log("Collection movies deleted");
         db.close();
@@ -22,7 +22,7 @@ function getPage() {
             response.results.map(result => (result.id = index++));
             MongoClient.connect(url, function (err, db) {
                 if (err) throw err;
-                var dbo = db.db('movieDB');
+                let dbo = db.db('movieDB');
                 dbo.collection('movies').insertMany(response.results, function (err, res) {
                     if (err) throw err;
                     console.log("page inserted");
@@ -37,7 +37,7 @@ function getPage() {
 async function asyncCall() {
     //cannot call page greater than 1000 from the api 
     if (i < 1001) {
-        var result = await getPage();
+        let result = await getPage();
         console.log(result);
         i++;
         asyncCall();
@@ -51,7 +51,7 @@ asyncCall();
 getJSON('https://api.themoviedb.org/3/genre/movie/list?api_key=9e72f98ad9d5c68503cf7a2b857f2b8e&language=en-US', function (error, response) {
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
-        var dbo = db.db('movieDB');
+        let dbo = db.db('movieDB');
         dbo.collection('genres').insertMany(response.genres, function (err, res) {
             if (err) throw err;
             console.log("Genres inserted");
